85d243d4af455e124bdb064a6cefcc981012e50f
Darjan Bogdan <bogdan.darjan@gmail.com>
2017-02-09 23:12:12 +0100

    Query arguments added

--- src/Reporter.Core/Query/IQuery.cs
+++ src/Reporter.Core/Query/IQuery.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reporter.Core.Query.Arguments;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Reporter.Core.Query
 {
     public interface IQuery<TResult>
     {
+        IQueryFilter Filter { get; set; }
+
+        IQuerySorter Sorter { get; set; }
+
+        IQueryPager Pager { get; set; }
+
+        IQueryOptions Options { get; set;}
     }
 }
